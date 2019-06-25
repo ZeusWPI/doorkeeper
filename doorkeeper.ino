e91f49c9238cd07e95df05cd3abcfc7072b42eeb
Francis Begyn <francis.begyn@gmail.com>
2019-06-25 18:12:45 +0200

    some formatting + delayed close button

--- doorkeeper.ino
+++ doorkeeper.ino
@@ -16,122 +16,121 @@ PWMServo door;
 // Skip all the HTTP headers, straight to the body
 bool skipRespHeaders(EthernetClient* client)
 {
-  char endOfHeaders[] = "\r\n\r\n";
-  bool skipped = client->find(endOfHeaders);
-  if(!skipped){
-  }
-  return skipped;
+    char endOfHeaders[] = "\r\n\r\n";
+    bool skipped = client->find(endOfHeaders);
+    if(!skipped){
+    }
+    return skipped;
 }
 
 // Authenticate against TOKEN
 bool authenticate(const char* token)
 {
-  if ((strncmp(token, MMTOKEN,64)==0) || (strncmp(token, SLACKTOKEN,64)==0))  {
-    return true;
-  } else {
-    return false;
-  }
+    if ((strncmp(token, MMTOKEN,64)==0) || (strncmp(token, SLACKTOKEN,64)==0))  {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 // verify if all json fields are present
 bool verifyData(DynamicJsonDocument* doc)
 {
-  bool userPresent = !(*doc)["username"].isNull();
-  bool tokenPresent = !(*doc)["token"].isNull();
-  bool textPresent = !(*doc)["text"].isNull();
-  if (userPresent && tokenPresent && textPresent) {
-    return true;
-  }
-  return false;
+    bool userPresent = !(*doc)["username"].isNull();
+    bool tokenPresent = !(*doc)["token"].isNull();
+    bool textPresent = !(*doc)["text"].isNull();
+    if (userPresent && tokenPresent && textPresent) {
+        return true;
+    }
+    return false;
 }
 
 // Process an incoming command request
 bool handleIncoming(char* command)
 {
-  EthernetClient client = server.available();
-  if(client && skipRespHeaders(&client)){
-    DynamicJsonDocument doc(256);
-    DeserializationError error = deserializeJson(doc, client);
-    if (error) {
-      client.println("HTTP/1.1 500 ERROR");
-      client.println("Content-Type: text/html");
-      client.println("Connection: close");
-      client.println();
-      client.println(error.c_str());
-      client.stop();
-      return false;
-    }
-    bool dataPresent = verifyData(&doc);
-    if (!dataPresent) {
-      client.println("HTTP/1.1 500 ERROR");
-      client.println("Content-Type: text/html");
-      client.println("Connection: close");
-      client.println();
-      client.println("Not all data fields were present");
-      client.stop();
-      return false;
-    }
-    const char* recvToken = doc["token"];
-    bool auth = authenticate(recvToken);
-    if (auth) {
-      //*command = doc["text"];
-      strncpy(command, doc["text"],8);
-      char* user = doc["username"];
-      char* msg = strcat(user,":");
-      char* rsp_msg = strcat(msg,command);
-      client.println("HTTP/1.1 200 OK");
-      client.println("Content-Type: text/html");
-      client.println("Connection: close");
-      client.println();
-      client.println(rsp_msg);
-      delay(10);
-      client.stop();
-      return true;
+    EthernetClient client = server.available();
+    if(client && skipRespHeaders(&client)){
+        DynamicJsonDocument doc(256);
+        DeserializationError error = deserializeJson(doc, client);
+        if (error) {
+            client.println("HTTP/1.1 500 ERROR");
+            client.println("Content-Type: text/html");
+            client.println("Connection: close");
+            client.println();
+            client.println(error.c_str());
+            client.stop();
+            return false;
+        }
+        bool dataPresent = verifyData(&doc);
+        if (!dataPresent) {
+            client.println("HTTP/1.1 500 ERROR");
+            client.println("Content-Type: text/html");
+            client.println("Connection: close");
+            client.println();
+            client.println("Not all data fields were present");
+            client.stop();
+            return false;
+        }
+        const char* recvToken = doc["token"];
+        bool auth = authenticate(recvToken);
+        if (auth) {
+            strncpy(command, doc["text"],8);
+            char* user = doc["username"];
+            char* msg = strcat(user,":");
+            char* rsp_msg = strcat(msg,command);
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-Type: text/html");
+            client.println("Connection: close");
+            client.println();
+            client.println(rsp_msg);
+            delay(10);
+            client.stop();
+            return true;
+        } else {
+            client.println("HTTP/1.1 403 UNAUTHORIZED");
+            client.println("Content-Type: text/html");
+            client.println("Connection: close");
+            client.println();
+            client.println("Invalid token");
+            delay(10);
+            client.stop();
+            return false;
+        }
     } else {
-      client.println("HTTP/1.1 403 UNAUTHORIZED");
-      client.println("Content-Type: text/html");
-      client.println("Connection: close");
-      client.println();
-      client.println("Invalid token");
-      delay(10);
-      client.stop();
-      return false;
+        return false;
     }
-  } else {
-    return false;
-  }
 }
 
 bool initEthernet()
 {
-  // Network configuration
-  byte mac[] = {0x90, 0xA2, 0xDA, 0x10, 0xFA, 0x91};    // MAC address
-  IPAddress ip(10, 0, 1, 5);                           // IP address
-  // Ethernet connection setup
-  Ethernet.begin(mac, ip);
-  return true;
+    // Network configuration
+    byte mac[] = {0x90, 0xA2, 0xDA, 0x10, 0xFA, 0x91};    // MAC address
+    IPAddress ip(10, 0, 1, 5);                           // IP address
+    // Ethernet connection setup
+    Ethernet.begin(mac, ip);
+    return true;
 }
 
 // Maintain ethernet connection
 void maintainEthernet()
 {
-  switch (Ethernet.maintain())
-  {
-    case 1:
-      Serial.println("Error: renewed fail");
-      break;
-    case 2:
-      Serial.println("Renewed success");
-      break;
-    case 3:
-      Serial.println("Error: rebind fail");
-      break;
-    case 4:
-      Serial.println("Rebind success");
-      break;
-    default:
-      break;
-  }
+    switch (Ethernet.maintain())
+    {
+        case 1:
+            Serial.println("Error: renewed fail");
+            break;
+        case 2:
+            Serial.println("Renewed success");
+            break;
+        case 3:
+            Serial.println("Error: rebind fail");
+            break;
+        case 4:
+            Serial.println("Rebind success");
+            break;
+        default:
+            break;
+    }
 }
 
 /*
@@ -146,31 +145,49 @@ void closeDoor()  {door.write(170);}
 void halt()       {door.write(94);}
 void lockDoor()
 {
-  closeDoor();
-  delay(2250);
-  halt();
+    closeDoor();
+    delay(2250);
+    halt();
+}
+void delayedClose(int dur)
+{
+    delay(dur);
+    lockDoor();
+}
+
+void triggerDelayedButton(void)
+{
+    Serial.println("Closing door");
+    delayedClose(1);
 }
 
 // Process a command
 void handleCommand(char* command, int operTime)
 {
-  if(strncmp(command,"open",8)==0){
-    openDoor();
-    delay(operTime);
-    halt();
-  } else if(strncmp(command,"close",8)==0) {
-    closeDoor();
-    delay(operTime);
-    halt();
-  } else if(strncmp(command,"lock",8)==0) {
-    lockDoor();
-  } else if(strncmp(command,"delay",8)==0) {
-    delay(15000);
-    lockDoor();
-  } else {
-    halt();
-  }
+    if(strncmp(command,"open",8)==0){
+        openDoor();
+        delay(operTime);
+        halt();
+    } else if(strncmp(command,"close",8)==0) {
+        closeDoor();
+        delay(operTime);
+        halt();
+    } else if(strncmp(command,"lock",8)==0) {
+        lockDoor();
+    } else if(strncmp(command,"delay",8)==0) {
+        delayedClose(10000);
+    } else {
+        halt();
+    }
 }
+
+// button variables
+const int buttonPin = 3;
+int buttonState;
+int lastButtonState = LOW;
+
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;
 
 // Global variables for the loop
 char rxCommand[256];
@@ -178,38 +195,49 @@ bool processed = false;
 
 void setup()
 {
-  //Serial.begin(9600);
-  //Serial.println("Booting up ...");
+    //Serial.begin(9600);
+    //Serial.println("Booting up ...");
 
-  //Serial.println("Initialising servo controller ...");
-  // Config servo motor control pin, 500-2500 is for the 10kg/cm servo
-  door.attach(SERVO_PIN_A);
-  halt();
+    //Serial.println("Initialising servo controller ...");
+    // Config servo motor control pin, 500-2500 is for the 10kg/cm servo
+    door.attach(SERVO_PIN_A);
+    halt();
 
-  //Serial.println("Initialising internet ...");
-  // Ethernet setup
-  while(!initEthernet()){
-    // If init fails, keep retrying
-  }
+    //Serial.println("Initialising internet ...");
+    // Ethernet setup
+    while(!initEthernet()){
+      // If init fails, keep retrying
+    }
 
-  //Serial.println("Starting webserver ...");
-  // Start the webserver
-  server.begin();
+    //Serial.println("Starting webserver ...");
+    // Start the webserver
+    server.begin();
 
-  //Serial.println("Doorkeeper: online");
+    // button for delayed close
+    pinMode(buttonPin, INPUT);
+
+    //Serial.println("Doorkeeper: online");
 }
 
 void loop()
 {
-  // Maintain the ethernet connection
-  maintainEthernet();
+    // Maintain the ethernet connection
+    maintainEthernet();
 
-  // Open/close doors
-  processed = handleIncoming(rxCommand);
-  if (processed) {
-      handleCommand(rxCommand, 1750);
-    processed = false;
-    delay(100);
-  }
-  delay(50);
+    // Open/close doors
+    processed = handleIncoming(rxCommand);
+    if (processed) {
+        handleCommand(rxCommand, 1750);
+        processed = false;
+        delay(100);
+    }
+
+    buttonState = digitalRead(buttonPin);
+    if (buttonState != lastButtonState) {
+        if (buttonState == LOW) {
+            delayedClose(10000);
+        }
+    }
+    lastButtonState = buttonState;
+    delay(50);
 }
